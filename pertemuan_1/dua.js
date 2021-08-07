ccb58fbe16272ba58419e38b8a2446bf5330dc60
sattama <satadhitama@gmail.com>
2021-08-07 19:45:25 +0700

    Challange Pertemuan 1

---
+++ pertemuan_1/dua.js
@@ -0,0 +1,43 @@
+let month = 2;
+
+switch (month) {
+  case 1:
+    console.log(month, '= January');
+    break;
+  case 2:
+    console.log(month, '= February');
+    break;
+  case 3:
+    console.log(month, '= March');
+    break;
+  case 4:
+    console.log(month, '= April');
+    break;
+  case 5:
+    console.log(month, '= Mei');
+    break;
+  case 6:
+    console.log(month, '= June');
+    break;
+  case 7:
+    console.log(month, '= July');
+    break;
+  case 8:
+    console.log(month, '= August');
+    break;
+  case 9:
+    console.log(month, '= September');
+    break;
+  case 10:
+    console.log(month, '= October');
+    break;
+  case 11:
+    console.log(month, '= November');
+    break;
+  case 12:
+    console.log(month, '= Desember');
+    break;
+  default:
+    console.log('Invalid Month');
+    break;
+}
---
+++ pertemuan_1/satu.js
@@ -0,0 +1,8 @@
+var fruitMatriks = [
+  ["Mango", "Apple"],
+  ["Nenas", "Kates"]
+]
+
+for (let i = 0; i < fruitMatriks.length; i++) {
+  console.log(`Saya suka ${fruitMatriks[i][0]} dan ${fruitMatriks[i][1]}`);
+}
